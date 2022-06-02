ea8e5dc9334b09437b8241c4d79f9e95192af0dc
alex hernandez <alexanderohernandez21@gmail.com>
2022-06-01 19:55:22 -0700

    added type def

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,46 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+    type User{
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]!
+    }
 
+    type Book{
+        bookId: ID
+        authors: [String]
+        description: String
+        bookId: String
+        title: String
+        link: String
+        image: String
+    }
+    type Auth{
+        token: ID!
+        user: User
+    }
+
+    input savedBook{
+        authors: [String]
+        description: String
+        bookId: String
+        title: String
+        link: String
+        image: String
+    }
+
+    type Query{
+        me: User
+    }
+
+    type Mutation{
+        addUser(username: String!, email: String!, password: String!):Auth
+        login(email: String!, password: String!): Auth
+        saveBook(input: savedBook!): User
+        deleteBook(bookId: ID): User
+    }
 `
+module.exports = typeDefs;
