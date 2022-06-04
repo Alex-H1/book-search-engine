aff67ae8abbdd53530712e0f2797e12672076577
alex hernandez <alexanderohernandez21@gmail.com>
2022-06-03 21:26:57 -0700

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks' || 'mongodb://127.0.0.1:27017/YD0V72plwcPRDt9u', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks' || 'mongodb://localhost:127.0.0.1:27017/YD0V72plwcPRDt9u', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
